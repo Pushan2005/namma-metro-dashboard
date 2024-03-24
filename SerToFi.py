9330dfdcb2e1c8fb3eb7ddb356e0a2d7fd68dd36
Pushan T <pushan.sahanaganesh@gmail.com>
2024-03-24 13:55:59 +0530

    py script to write serial monitor logs to csv

--- SerToFi.py
+++ SerToFi.py
@@ -1,13 +1,15 @@
 import serial as Serial
 import time
+import csv
 
 # Set up the serial connection (COM port may vary)
-ser = Serial.Serial('COM3', 9600, timeout=1)
+ser = Serial.Serial('COM10', 9600, timeout=1)
 time.sleep(2)  # wait for the connection to initialize
 
-with open('rfid_data.txt', 'w') as file:
+with open('rfid_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
-            file.write(line + '\n')
+            writer.writerow([line])
